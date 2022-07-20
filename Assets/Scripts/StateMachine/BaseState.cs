a409103c02c4e5b12cc2e8433d26572213d0e1e2
DanielSleczka <danielsleczkads@gmail.com>
2022-07-20 12:56:01 +0200

    base script of machine states added

---
+++ Assets/Scripts/StateMachine/BaseState.cs
@@ -0,0 +1,19 @@
+
+public abstract class BaseState
+{
+    public virtual void InitializeState()
+    {
+
+    }
+
+    public virtual void UpdateState()
+    {
+
+    }
+
+    public virtual void DestroyState()
+    {
+
+    }
+
+}
